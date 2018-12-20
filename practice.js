3a5ab56286d21f24196c417545127774f239250e
davepodgorski <44556558+davepodgorski@users.noreply.github.com>
2018-12-20 17:39:06 -0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,96 @@
+// const apples = 14;
+// console.log(apples);
+//
+// console.log (`I have ${apples} apples.`);
+//
+// console.log ('________________');
+//
+// const materials = ['wood', 'metal', 'stone'];
+// words = {
+//   elephant: "The world's largest land mammal.",
+//   school: 'A place of learning.',
+//   iceCream: 'A delicious milk-based dessert.'
+// };
+//
+// const num = 16;
+// if (num > 10) {
+//   console.log(`${num} is greater than 10.`);
+// }else if (num === 10) {
+//   console.log(`${num} is exactly 10.`);
+// }else {
+//   console.log(`${num} must be less than 10.`);
+// }
+// console.log ('________________');
+//
+// function timesDo(){ console.log("Doing the same thing over and over.") }
+//
+// [1,2,3,4,5,6,7,8,9,10].forEach(function(i) {
+//   timesDo();
+// });
+//
+// console.log ('________________');
+
+// const base = 5;
+// const times = 20;
+// for(let num = 0; num <= times; num ++) {
+//     console.log(num + base);
+// }
+
+//
+// const base = 5;
+// 20.times do |num|
+//   puts num + base
+// end
+
+// console.log ('________________');
+
+
+// const total = 0;
+// const timess = 100;
+// for(let num = 0; num <= timess; num ++){
+//   console.log(num + total);
+// }
+
+// 100.times do |num|
+//   total += num
+// end
+// puts total
+//
+//
+// const max = 15;
+// for(let num = 3; num <= max; num ++) {
+// if (num > 9) {
+//     console.log('You can get on the rollercoaster!');
+//   }
+//   else {
+//     console.log('You are too short to ride this rollercoaster.');
+//   }
+// }
+
+// (3..15).each do |height|
+//   if height > 9
+//     puts "You can get on the rollercoaster!"
+//   else
+//     puts "You are too short to ride this rollercoaster."
+//   end
+// end
+
+//
+// const containers = ['purse', 'wallet', 'backback'];
+// for (let i = 0; i < containers.length; i ++) {
+//   console.log(`${containers[i]}`);
+// }
+// // containers.each do |container|
+// //   puts container
+// end
+
+// function helloWorld(){console.log("Hello world!") }
+//
+// helloWorld()
+
+function add(first_num, second_num){
+return  first_num + second_num;
+}
+
+const amount = add(5, 7);
+console.log(amount);
